6bb7edf38a539df4b73e06f075c5ab8efa764083
shivamrai83 <shivamrai83@gmail.com>
2020-12-27 00:43:38 +0530

    Infinite Scroll Added

--- src/issue/table.jsx
+++ src/issue/table.jsx
@@ -1,16 +1,41 @@
 import React,{useEffect,useState} from 'react'
-import axios from 'axios'
+import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 import '../App.css'
 
 export default function Table() {
     const [arr,setArr] = useState([]);
+    const [page,setPage] =useState(1);
+    const [loading,setLoading]=useState(true);
+
     useEffect(async () => {
-       const data = await axios.get('https://api.github.com/repos/facebook/react/issues?page=1')
-       setArr(data.data)
-    }, [])
+       const data = await axios.get(`https://api.github.com/repos/facebook/react/issues?page=${page}`)
+       if(page>1){
+           const setArray=[...arr,...data.data]
+           setArr(setArray);
+       }
+      
+       else{
+        setArr(data.data)
+       }
+       setLoading(false);
+    }, [page])
+
+    
+    function LoadMoreData(e){
+        let reachedBottom= e.target.scrollHeight - e.target.scrollTop - e.target.clientHeight  < 50;
+        if(page>23){
+            reachedBottom=false;
+        }
+        if(reachedBottom){
+            setPage(page+1);
+            console.log(page);
+        }
+    }
 
     return (
-        <div className="table">
+        <div onScroll={LoadMoreData} className="table">       
             {
                 arr.map(val=>(
                     <table className="tables">
@@ -22,6 +47,8 @@ export default function Table() {
                     </table>
                 ))
             }
-        </div>
+        
+      
+         </div>
     )
 }
